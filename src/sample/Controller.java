4be753450dc537c942dd8526a586de80d6af1b8d
kruvv <kruvv@mail.ru>
2019-01-09 14:53:21 +0300

    Создание дополнительного окна

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authSignButton;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField loginField;
+
+
+
+    @FXML
+    void initialize() {
+        authSignButton.setOnAction(event -> {
+            System.out.println("You press key on button < Come in >");
+        });
+
+    }
 }
+
