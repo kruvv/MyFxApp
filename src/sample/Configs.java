01c8631e4c8475912b5af4714126935e00a3c401
kruvv <kruvv@mail.ru>
2019-01-10 19:51:37 +0300

    Создание БД, настройка подключения к ней.

---
+++ src/sample/Configs.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "test";
+    protected String dbPass = "test";
+    protected String dbName = "vivat";
+}
--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,11 +1,16 @@
 package sample.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -31,10 +36,40 @@ public class Controller {
 
     @FXML
     void initialize() {
+
         authSignButton.setOnAction(event -> {
-            System.out.println("You press key on button < Come in >");
+            String loginText = loginField.getText().trim();
+            String loginPassword = passwordField.getText().trim();
+
+            if(!loginText.equals("") && !loginPassword.equals("")) {
+                loginUser(loginText, loginPassword);
+            }else {
+                System.out.println("Login and Password is empty");
+            }
         });
 
+
+        loginSignUpButton.setOnAction(event -> {
+            loginSignUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/view/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+    }
+
+    private void loginUser(String loginText, String loginPassword) {
     }
 }
 
